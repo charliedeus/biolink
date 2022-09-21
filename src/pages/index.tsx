d94d4142645e2c5969c0d96e775b06c6181719a2
charliedeus <charliedeus@gmail.com>
2022-09-21 17:19:16 -0300

    implementing css base

---
+++ src/pages/index.tsx
@@ -0,0 +1,115 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import { BigHead } from '@bigheads/core'
+import {
+  WhatsappLogo,
+  TwitterLogo,
+  YoutubeLogo,
+  InstagramLogo,
+  EnvelopeSimple,
+  FacebookLogo,
+  House,
+} from 'phosphor-react'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Liz Midlej Silva</title>
+      </Head>
+
+      <header className="w-full text-center flex flex-col items-center">
+        <div className="w-[12em] h-[12em]">
+          <BigHead
+            accessory="roundGlasses"
+            body="chest"
+            circleColor="blue"
+            clothing="dress"
+            clothingColor="blue"
+            eyebrows="concerned"
+            eyes="content"
+            faceMask={false}
+            faceMaskColor="blue"
+            facialHair="none"
+            graphic="none"
+            hair="pixie"
+            hairColor="blonde"
+            hat="none"
+            hatColor="black"
+            lashes
+            lipColor="red"
+            mask
+            mouth="openSmile"
+            skinTone="light"
+          />
+        </div>
+        <h1 className="font-bold text-xl inline-block bg-gray-800 text-white p-[0.3em_0.6em] rounded-full border-2 border-gray-100 backdrop-blur backdrop-saturate-150 backdrop-contrast-150">
+          Liz Midlej Silva
+        </h1>
+      </header>
+
+      <ul className="p-8 m-auto">
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <House size={20} weight="light" />
+              <span>Site Oficial</span>
+            </a>
+          </Link>
+        </li>
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <WhatsappLogo size={20} weight="light" />
+              <span>WhatsApp</span>
+            </a>
+          </Link>
+        </li>
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <TwitterLogo size={20} weight="light" />
+              <span>Twitter</span>
+            </a>
+          </Link>
+        </li>
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <YoutubeLogo size={20} weight="light" />
+              <span>Youtube</span>
+            </a>
+          </Link>
+        </li>
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <InstagramLogo size={20} weight="light" />
+              <span>Instagram</span>
+            </a>
+          </Link>
+        </li>
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <EnvelopeSimple size={20} weight="light" />
+              <span>E-mail</span>
+            </a>
+          </Link>
+        </li>
+        <li className="bg-gray-50 backdrop-blur backdrop-saturate-150 backdrop-contrast-150 rounded-[10em] p-[1em] m-[1.4em_0]">
+          <Link href="/">
+            <a className="flex items-center justify-center gap-2">
+              <FacebookLogo size={20} weight="light" />
+              <span>Facebook</span>
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif',
+      },
+      backgroundImage: {
+        'hero-patter':
+          "url('https://images.unsplash.com/photo-1580508174046-170816f65662?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')",
+      },
+    },
+  },
+  plugins: [],
+}
